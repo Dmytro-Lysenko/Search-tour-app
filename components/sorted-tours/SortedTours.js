8890b50c1b383ee766c2255b3b76576be1bafee9
Dmytro Lysenko <crystaljoint@gmail.com>
2022-01-18 21:33:36 +0200

    add styles to sortedTours component

--- components/sorted-tours/SortedTours.js
+++ components/sorted-tours/SortedTours.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+import classes from "./SortedTours.module.css";
+
 function SortedTours(props) {
+  const sortHandler = (value, event) => {
+    props.onSort(value);
+  };
+
   return (
-    <div>
-      <button onClick={() => props.onSort("date")}>By date</button>
-      <button onClick={() => props.onSort("price")}>By price</button>
-      <button onClick={() => props.onSort("country")}>By country</button>
+    <div className={classes.container}>
+      <button className={classes.button} onClick={() => sortHandler("date")}>
+        By date
+      </button>
+      <button className={classes.button} onClick={() => sortHandler("price")}>
+        By price
+      </button>
+      <button className={classes.button} onClick={() => sortHandler("country")}>
+        By country
+      </button>
     </div>
   );
 }
